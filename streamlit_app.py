7f394e6c4350797f45193002c8275f414b147d4f
crf0209 <127452517+crf0209@users.noreply.github.com>
2023-03-15 22:56:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
+
 import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
@@ -46,3 +49,4 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 #insert values from python to snowflake table
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
